0d723126cfe2b7da1d3cf7e4009166f94eb72b7d
temiijoy-ctrl <temiijoy@gmail.com>
2026-09-12 15:44:36 -0400

    bo
    boo

---
+++ Assets/Magic Pig Games (Infinity PBR)/Medieval Environment Pack/_Demo Scenes/Demo Scripts/IPBR_EnvironmentWarning.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class IPBR_EnvironmentWarning : MonoBehaviour
+{
+    [SerializeField] private string folderPath = "assets/BOTD_Standard Shader";
+    [SerializeField] private string folderPathURP =  "assets/Book of the Dead - URP";
+    public GameObject panel;
+    #if UNITY_EDITOR
+    
+    private void Awake()
+    {
+        if (AssetDatabase.IsValidFolder(folderPath) || AssetDatabase.IsValidFolder(folderPathURP))
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        
+        panel.SetActive(true);
+    }
+
+    public void GoToAssetStore() => Application.OpenURL("https://assetstore.unity.com/packages/book-of-the-dead-urp/369748?aid=1100lxWw");
+
+    #endif
+}
